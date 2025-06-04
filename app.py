23167e07cc81a53b37ac8567ced6a4b6e2d41b0b
anuprath87 <38036120+anuprath87@users.noreply.github.com>
2025-06-04 09:06:25 +0530

    PA1

---
+++ app.py
@@ -0,0 +1,22 @@
+import streamlit as st
+import pandas as pd
+from sklearn.linear_model import LinearRegression
+
+st.title("Linear Regression with Streamlit")
+
+#data = pd.read_csv("data.csv")
+#st.write(data)
+
+# Sidebar - Upload CSV
+st.sidebar.header("Upload Your CSV File")
+data = st.sidebar.file_uploader("Upload CSV", type=["csv"])
+
+x = data[['Hours_Studied']]
+y = data['Marks_Scored']
+
+model = LinearRegression()
+model.fit(x, y)
+
+hours = st.number_input("Enter Hours Studied:", 1.0, 10.0, step=0.5)
+prediction = model.predict([[hours]])
+st.write("Predicted Marks:", prediction[0])
